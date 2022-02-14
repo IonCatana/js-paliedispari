269b9626e8be7c56363009be6267ef8ebde25c06
IonCatana <catana.ion17@yahoo.it>
2022-02-14 21:19:21 +0100

    Pari e Dispari finito

--- Pari e Dispari/app.js
+++ Pari e Dispari/app.js
@@ -1,33 +1,35 @@
-
-
 // **************** Pari e Dispari*****************
 
 // Creare le varibile
-let numeroUtente;
-let numeroPc;
-console.log(numeroUtente)
 
-numeroUtente = prompt('Inserisci il numero');
-numeroUtente = numeroUtente * 1;
-if (numeroUtente < 0) {
-  numeroUtente = -numeroUtente;
-}
-numeroUtente;
-while (numeroUtente > 1) {
-  numeroUtente = numeroUtente - 2;
-}
-if (numeroUtente == 0) {
-  alert('Il numero è pari');
-} else {
-  alert('Il numero è dispari');
-}
-
-function getRandomInt(1, 5) {
-  min = Math.ceil(min);
-  max = Math.floor(max);
-  return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
-}
 // L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
-// Generiamo un numero random(sempre da 1 a 5) per il computer(usando una funzione).
-// Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari(usando una funzione)
+const decisione = prompt('Scegli: pari o dispari?');
+const nUtente = parseInt(prompt('Scegli un numero da 1 a 5'));
+console.log(nUtente, 'Numero scelto dall nUtente');
+
+// Generiamo un numero random(sempre da 1 a 5) per il computer usando la nostra funzione.
+const nComputer = random(1, 5);
+function random(min, max) {
+  min = parseInt(min);
+  max = parseInt(max);
+  return parseInt(Math.random() * (max - min) + min);
+}
+console.log(nComputer, 'Numero generato random dall nComputer');
+
+// Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione)
+const risultato = somma(nUtente, nComputer);
+function somma(num1, num2) {
+  let somma = num1 + num2;
+  return somma;
+}
+console.log(risultato, 'Somma totale (nUtente + nComputer)');
+
 // Dichiariamo chi ha vinto.
+if (
+  (decisione === 'pari' && risultato % 2 == 0) ||
+  (decisione === 'dispari' && risultato % 2 !== 0)
+) {
+  alert('hai vinto');
+} else {
+  alert('hai perso');
+}
